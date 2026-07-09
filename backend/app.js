aaca662ffea402a598e55b8baad02b28d89a7cf8
riakrst <riakristibasri@gmail.com>
2026-07-10 01:29:15 +0700

    add showtime CRUD endpoints

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const authRoutes = require("./routes/authRoutes");
 const locationRoutes = require("./routes/locationRoutes");
 const bioskopRoutes = require("./routes/bioskopRoutes");
 const movieRoutes = require("./routes/movieRoutes");
+const showtimeRoutes = require("./routes/showtimeRoutes");
 
 const app = express();
 
@@ -15,6 +16,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/locations", locationRoutes);
 app.use("/api/bioskop", bioskopRoutes);
 app.use("/api/movies", movieRoutes);
-
+app.use("/api", showtimeRoutes);
 
 module.exports = app;
