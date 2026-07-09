48cd64caf40134dc392053b77f7ae4833070c126
zet-nbs <fairizet@gmail.com>
2026-07-09 10:13:44 +0700

    Implement movie CRUD operations with routing and middleware protection

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
+const movieRoutes = require("./routes/movieRoutes");
 
 const app = express();
 
-// Middleware untuk membaca JSON dan Cookies dari request
+
 app.use(express.json());
 app.use(cookieParser());
 
-// Pendaftaran Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/movies", movieRoutes);
+
 
 module.exports = app;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,7 @@ const { register, login, getMe } = require("../controllers/authController");
 const rateLimit = require("express-rate-limit");
 const { protect } = require("../middleware/authMiddleware");
 
+
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // Waktu: 15 Menit
   max: 5, // Maksimal percobaan: 5 kali
